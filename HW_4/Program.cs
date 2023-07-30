93a34f62b9a6a6c9c199ccd2a45ff32c44661804
vika <vmliechko@mail.ru>
2023-07-30 15:11:10 +0300

    домашняя работа 4

---
+++ HW_4/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите степень:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// int GetMult()
+// {
+//     int newNum=1;
+//     int count = 1;
+//     while (count<=num1)
+//     {
+//     newNum = newNum*num;
+//     count++;
+//     }
+//     return newNum;
+// }
+// int result = GetMult();
+// Console.WriteLine(result);
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и
+//  выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+// int[] array = new int[8];
+
+// void GetArray()
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i]= new Random().Next(0,100);
+//         Console.Write(array[i]+" ");
+//     }
+// }
+
+// GetArray();
+
+// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int GetSumNum()
+// {
+//     int sum = 0;
+//     while (num>0)
+//     {
+//         sum=sum+num%10;
+//         num/=10;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine(GetSumNum());
+
+
