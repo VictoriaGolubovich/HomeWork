7af737ab2974492541e5337501e8607cc0fb58cc
vika <vmliechko@mail.ru>
2023-09-05 13:02:41 +0300

    домашняя работа 9

---
+++ HW_9/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+// Console.WriteLine("Введите число"); 
+// int num = Convert.ToInt32(Console.ReadLine()); 
+
+// int endNum =1; 
+// string PrintNumber(int start, int end) 
+// {   
+//     if(start==end)   
+//     {     
+//         return start.ToString();   
+//     }   
+//     return (start + " "+PrintNumber(start-1, end));
+// } 
+// Console.WriteLine(PrintNumber(num,endNum));
+
+
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+// System.Console.WriteLine("Введите число M"); 
+// int num = Convert.ToInt32(System.Console.ReadLine()); 
+
+// System.Console.WriteLine("Введите число N"); 
+// int num2 = Convert.ToInt32(System.Console.ReadLine()); 
+
+// if (num < num2) 
+// {     
+//     Console.WriteLine(PrintNum(num,num2)); 
+// } 
+// else 
+// {     
+//     System.Console.WriteLine("Введите число N больше M"); 
+// } 
+
+// int PrintNum(int start, int end) 
+
+// {     
+//     if (start == end)     
+//     {         
+//         return start;     
+//     }     
+//     return(start + PrintNum(start+1, end)); 
+// }
+
+
+
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+
+// Console.WriteLine("Введите число"); 
+// int num = Convert.ToInt32(Console.ReadLine()); 
+
+// Console.WriteLine("Введите степень числа"); 
+// int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+// int Pow(int m, int n) 
+// {   
+//     if(m==0)   
+//     {     
+//         return n+1;   
+//     }   
+//     if(m==1)   
+//     {     
+//         return n+2;   
+//     }   
+//     if(m==2)   
+//     {     
+//         return 2*n+3; 
+//     }   
+//     return ((m * Pow(m, n-1))-3); 
+// } 
+// Console.WriteLine(Pow(num,num2));
